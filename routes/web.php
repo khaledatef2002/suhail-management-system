1dbda40ccd1192c3adbaf149974098a6fb8606ea
Khaled Atef <39334335+khaledatef2002@users.noreply.github.com>
2025-03-22 22:23:08 +0200

    no auth required for internship form

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 require_once __DIR__ . '/dashboard.php';
 require_once __DIR__ . '/auth.php';
 
-Route::name('front.')->prefix(LaravelLocalization::setLocale())->middleware(['localeSessionRedirect', 'localizationRedirect', 'localeViewPath','auth'])->group(function(){
+Route::name('front.')->prefix(LaravelLocalization::setLocale())->middleware(['localeSessionRedirect', 'localizationRedirect', 'localeViewPath'])->group(function(){
     Route::get('internship-apply', [InternShipController::class, 'index'])->name('internship-apply');
     Route::post('internship-apply', [InternShipController::class, 'store'])->name('internship-apply');
 });
