0a87635693e686433b5a1219eacad3dd097c8df6
Khaled Atef <39334335+khaledatef2002@users.noreply.github.com>
2025-03-24 19:33:40 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,10 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 require_once __DIR__ . '/dashboard.php';
 
-Route::name('front.')->prefix(LaravelLocalization::setLocale())->middleware(['localeSessionRedirect', 'localizationRedirect', 'localeViewPath'])->group(function(){
+Route::prefix(LaravelLocalization::setLocale())->middleware(['localeSessionRedirect', 'localizationRedirect', 'localeViewPath'])->group(function(){
     require_once __DIR__ . '/auth.php';
-    Route::get('internship-apply', [InternShipController::class, 'index'])->name('internship-apply');
-    Route::post('internship-apply', [InternShipController::class, 'store'])->name('internship-apply');
+    Route::name('front.')->group(function(){
+        Route::get('internship-apply', [InternShipController::class, 'index'])->name('internship-apply');
+        Route::post('internship-apply', [InternShipController::class, 'store'])->name('internship-apply');
+    });
 });
